7b039b3d081e004bf63428fef4978fea2f8336d0
osman yalçın <osmanyalcin989@gmail.com>
2023-05-03 17:05:20 +0300

    coordinated

--- lib/Server.hpp
+++ lib/Server.hpp
@@ -10,6 +10,14 @@ class Server : public Socket
 
 		int is_nickname_exist(std::string nick) const;
 		int is_channel_active(std::string);
+		void command_user(int index, std::vector<std::string> array);
+		void command_privmsg(int index, std::vector<std::string> array);
+		void command_join(int index, std::vector<std::string> array);
+		void command_topic(int index, std::vector<std::string> array);
+		void command_who(int index, std::vector<std::string> array);
+		void command_part(int index, std::vector<std::string> array);
+		void command_quit(int index);
+
 	public:
 		std::vector<struct pollfd> pfds;
 		std::vector<User> user_list;
@@ -19,25 +27,32 @@ class Server : public Socket
 		~Server();
 
 		void acception();
-		
-		void delete_fd(int index);
-		void create_fd(int fd);
-		
-		void delete_user(int index);
-		void create_user(std::string user_name, std::string nick_name, std::string real_name, int fd);
-
 		void getting_command(int index, std::string msg);
+		
 		std::vector<std::string> parse(std::string input);
 		std::string create_msg(int index, std::string code, std::string msg);
 		std::string create_msg_2(int index, std::string msg);
 		void user_to_user(int index, std::string command, std::string receiver_nick_name, std::string msg);
 		void send_msg(int fd, std::string msg);
 	
-		std::string get_host_name();
-
+		//CHANNEL
 		Channel &find_channel(std::string name);
 		void remove_from_all_channels(User user, int index);
 
+		// USER/FD CREATE/DELETE
+		void delete_fd(int index);
+		void create_fd(int fd);
+		
+		void delete_user(int index);
+		void create_user(std::string user_name, std::string nick_name, std::string real_name, int fd);
+
+		//GETTER
+		std::string get_host_name();
+
+		//SETTER
+		void set_time();
+		void set_host_name();
+		
 		// DEBUG
 		void print_info() const;
 };
--- src/Server.cpp
+++ src/Server.cpp
@@ -5,193 +5,43 @@ Server::Server(int port) : Socket::Socket(port)
 	fail_check(bind(fd, (struct sockaddr *)&address, sizeof(address)));
 	fail_check(listen(fd, MAX_CLIENT));
 	create_fd(fd);
-
-	//host_name
-	char tmp[255];
-	gethostname(tmp, 255);
-	host_name = tmp;
-
-	//time
-	time_t rawtime;
-	struct tm *timeinfo;
-	char buffer[80];
-
-	std::time(&rawtime);
-	timeinfo = localtime(&rawtime);
-
-	strftime(buffer, 80, "%d-%m-%Y %I:%M:%S", timeinfo);
-	created_time = std::string(buffer);
 }
 
-Server::~Server()
-{
+Server::~Server(){}
 
-}
-
-void Server::acception()
-{
+void Server::acception(){
 	int client_fd = accept(fd, 0, 0);
 	fail_check(client_fd);
 	create_fd(client_fd);
 }
 
-void Server::create_user(std::string user_name, std::string nick_name, std::string real_name, int fd){
-	if (is_nickname_exist(nick_name) == -1) {
-		User new_user(user_name, nick_name, real_name, fd);
-		user_list.push_back(new_user);
-	} else {
-		// send error code
-	}
-}
-
-void Server::delete_user(int index) {
-		user_list.erase(user_list.begin() + USER_ID);
-}
-
-void Server::create_fd(int fd) {
-	struct pollfd temp;
-	temp.fd = fd;
-	temp.events = POLLIN;
-	temp.revents = 0;
-
-	pfds.push_back(temp);
-}
-
-void Server::delete_fd(int index) {
-	//delete index
-	close(pfds[index].fd);
-	pfds.erase(pfds.begin() + index);
-}
-
 void Server::getting_command(int index, std::string buffer) {
 
-    std::vector<std::string> array;
-
-		std::stringstream ss(buffer);
-		std::string word;
-		while (ss >> word)
-		{
-			array.push_back(word);
-		}
-		
+	std::vector<std::string> array;
+	array = parse(buffer);
 	int i = 0;
-	// std::cout << "start of vector" << std::endl;
+
 	for (std::vector<std::string>::iterator it = array.begin(); it != array.end(); ++it) {
 		std::cout << "i:" << i << ", value: " << *it << std::endl;
 		i++;
 	}
-	// std::cout << "end of vector" << std::endl;
 
 	if (array[0] == "USER")
-	{
-		// :osman 001 osman :Hi, welcome to IRC\r\n
-		// :osman 002 osman :Your host is osman, running version miniircd-2.2\r\n
-		// :osman 003 osman :This server was created sometime\r\n
-		// :osman 004 osman osman miniircd-2.2 o o\r\n
-		// :osman 251 osman :There are 1 users and 0 services on 1 server\r\n
-
-		create_user(array[1], array[6], array[4].substr(1), pfds[index].fd);
-		send_msg(pfds[index].fd, create_msg(index, "001", ":Hi, welcome to IRC"));
-		send_msg(pfds[index].fd, create_msg(index, "002", ":Your host is " + host_name + ", running version v1"));
-		send_msg(pfds[index].fd, create_msg(index, "003", ":This server was created " + created_time));
-		send_msg(pfds[index].fd, create_msg(index, "004", host_name + " v1 o o"));
-		send_msg(pfds[index].fd, create_msg(index, "251", ":There are " + std::to_string(user_list.size()) + " users and 0 services on 1 server"));
-	}
-	else if (array[0] == "PRIVMSG" || array[0] == "NOTICE"){
-		// user
-		// PRIVMSG abullah :41\r\n
-		// :osman 401 user_osman abullah :No such nick/channel\r\n
-		// PRIVMSG abdullah :61\r\n
-		// :user_osman!nick_os23@127.0.0.1 PRIVMSG abdullah :61\r\n /to abdullah
-		
-		// channel
-		// PRIVMSG #aynen :sdadsa\r\n 
-		// :abdullah!abdullah@127.0.0.1 PRIVMSG #aynen :sdadsa\r\n' to other user
-		// :abdullah!abdullah@127.0.0.1 PRIVMSG #aynen :sdadsa\r\n to other user
-
-		if (array[1].substr(0,1) == "#") //channel-user
-			find_channel(array[1]).send_message(user_list[USER_ID], create_msg_2(index, array[0] + " " + array[1] + " " + array[2]));
-		else if (is_nickname_exist(array[1]) != -1) //user-user
-			user_to_user(index, array[0], array[1], array[2]);
-		else
-			send_msg(pfds[index].fd, create_msg(index, "401", array[1] + " :No such nick/channel"));
-
-	}
+		command_user(index, array);
+	else if (array[0] == "PRIVMSG" || array[0] == "NOTICE")
+		command_privmsg(index, array);
 	else if (array[0] == "JOIN")
-	{
-		//	first join message
-		// ":abdullah!abdullah@192.168.1.34 JOIN #aynen\r\n"; 
-		// ":buraks-mbp.home 331 mat #mati :No topic is set\r\n";
-		// ":buraks-mbp.home 353 mat = #mati :mat\r\n";
-		// ":buraks-mbp.home 366 mat #mati :End of NAMES list\r\n";
-		// gelen kişi JOIN sonrası WHO mesaji gönderir
-
-		// channeldaki her adam için GELEN kişinin infosunu göndermek lazım 1 satır
-		// ":abdullah!abdullah@192.168.1.34 JOIN #aynen\r\n"; to everyone
-
-		int channel_index = is_channel_active(array[1]);
-		if (channel_list[channel_index].user_exists(user_list[USER_ID]))
-			return ;
-		channel_list[channel_index].add_to_channel(user_list[USER_ID]);
-		send_msg(pfds[index].fd, create_msg_2(index, array[0] + " " + array[1])); 
-		send_msg(pfds[index].fd, create_msg(index, "331", array[1] + " :" + channel_list[channel_index].get_topic()));
-		send_msg(pfds[index].fd, create_msg(index, "353", "= " + array[1] + " :" + channel_list[channel_index].get_str_user_list()));
-		send_msg(pfds[index].fd, create_msg(index, "366", array[1] + " :End of NAMES list"));
-		channel_list[channel_index].send_message(user_list[USER_ID], create_msg_2(index, array[0] + " " + array[1]));
-	}
-	else if (array[0] == "TOPIC"){
-		Channel channel = find_channel(array[1]);
-		channel.set_topic(array[2].substr(1));
-		std::string message = create_msg_2(index, "TOPIC " + array[1] + " " + array[2]);
-		channel.send_message(user_list[USER_ID], message);
-		write(user_list[USER_ID].get_fd(), message.c_str(), message.length());
-	}
-	else if (array[0] == "ISON"){
-			
-	}
-	else if (array[0] == "WHO"){
-			//who join hemen sonrası atılır gelme şekli: WHO #<channel_name>
-			//sadece o an giren kişiye yollanır diğerlerine özel tek satırlık Join mesajı yollanır
-
-			// nick_osman tek başına odaya girdikten sonra gönderilecek kod parçası
-			// :osman 352 nick_osman #aynen user_os23 127.0.0.1 osman nick_osman H :0 real_osyalcin\r\n
-			// :osman 315 nick_osman #aynen :End of WHO list\r\n
-
-			//odada nick_osman varken abdullah odaya girdikten sonra gönderilecek kod parçası
-			// :osman 352 abdullah #aynen user_os23 127.0.0.1 osman nick_osman H :0 real_osyalcin\r\n 
-			// :osman 352 abdullah #aynen abdullah 127.0.0.1 osman abdullah H :0 abdullah\r\n
-			// :osman 315 abdullah #aynen :End of WHO list\r\n
-
-			Channel channel = find_channel(array[1]);
-		 	std::vector<User> channels_user = channel.get_user_list();
-			for (std::vector<User>::const_iterator it = channels_user.begin(); it != channels_user.end(); ++it) {
-				send_msg(pfds[index].fd, create_msg(index, "352", (*it).get_user_name() + " " + host_name + " " + host_name + " " + (*it).get_nick_name() + " H :0 " + (*it).get_real_name())); //H AND :0 could change
-			}
-			send_msg(pfds[index].fd, create_msg(index, "315", array[1] + " :End of WHO list"));
-	}
-	else if (array[0] == "PING"){ //MAYBE
-			
-	}
-	else if (array[0] == "PART"){
-		// PART #aynen\r\n'
-		// :nick_osman!user_os23@127.0.0.1 PART #aynen :nick_osman\r\n'
-		// :nick_osman!user_os23@127.0.0.1 PART #aynen :nick_osman\r\n'
-		// :nick_osman!user_os23@127.0.0.1 PART #aynen :nick_osman\r\n'
-
-		find_channel(array[1]).remove_from_channel(user_list[USER_ID]);
-		std::vector<User> channels_user = find_channel(array[1]).get_user_list();
-		for (std::vector<User>::const_iterator it = channels_user.begin(); it != channels_user.end(); ++it) {
-			send_msg((*it).get_fd(), create_msg_2(index, array[0] + " " + array[1] + " :" + user_list[USER_ID].get_nick_name()));
-		}
-	}
+		command_join(index, array);
+	else if (array[0] == "TOPIC")
+		command_topic(index, array);
+	else if (array[0] == "WHO")
+		command_who(index, array);
+	else if (array[0] == "PART")
+		command_part(index, array);
 	else if (array[0] == "QUIT")
-	{
-		// send to all user
-		// nick_osman!user_os23@127.0.0.1 QUIT :Leaving.\r\n
-		remove_from_all_channels(user_list[USER_ID], index);
-		delete_fd(index);
-		delete_user(index);
-	}
+		command_quit(index);
+	else 
+		send_msg(pfds[index].fd, create_msg(index, "421", array[0] + " :Unknown command"));
 }
 
 std::vector<std::string> Server::parse(std::string input) {
@@ -230,8 +80,6 @@ std::vector<std::string> Server::parse(std::string input) {
 	return result;
 }
 
-
-
 void Server::user_to_user(int index, std::string command, std::string receiver_nick_name, std::string msg){
 	int user_index = is_nickname_exist(receiver_nick_name);
 	
@@ -252,10 +100,6 @@ void Server::send_msg(int fd, std::string msg) {
 	write(fd, msg.c_str(), msg.length());
 }
 
-std::string Server::get_host_name() {
-	return host_name;
-}
-
 int Server::is_nickname_exist(std::string nick) const
 {
 	int i = 0;
@@ -266,6 +110,8 @@ int Server::is_nickname_exist(std::string nick) const
 	}
 	return -1;
 }
+
+// CHANNEL_METHODS
 
 int Server::is_channel_active(std::string channel)
 {
@@ -297,6 +143,125 @@ void Server::remove_from_all_channels(User user, int index)
 		}
 		(*it).remove_from_channel(user);
 	}
+}
+
+
+// PRIVATE FUNCTIONS
+void Server::command_user(int index, std::vector<std::string> array){
+	create_user(array[1], array[6], array[4].substr(1), pfds[index].fd);
+	send_msg(pfds[index].fd, create_msg(index, "001", ":Hi, welcome to IRC"));
+	send_msg(pfds[index].fd, create_msg(index, "002", ":Your host is " + host_name + ", running version v1"));
+	send_msg(pfds[index].fd, create_msg(index, "003", ":This server was created " + created_time));
+	send_msg(pfds[index].fd, create_msg(index, "004", host_name + " v1 o o"));
+	send_msg(pfds[index].fd, create_msg(index, "251", ":There are " + std::to_string(user_list.size()) + " users and 0 services on 1 server"));
+}
+
+void Server::command_privmsg(int index, std::vector<std::string> array){
+	if (array[1].substr(0,1) == "#") //channel-user
+		find_channel(array[1]).send_message(user_list[USER_ID], create_msg_2(index, array[0] + " " + array[1] + " " + array[2]));
+	else if (is_nickname_exist(array[1]) != -1) //user-user
+		user_to_user(index, array[0], array[1], array[2]);
+	else
+		send_msg(pfds[index].fd, create_msg(index, "401", array[1] + " :No such nick/channel"));
+
+}
+
+void Server::command_join(int index, std::vector<std::string> array){
+	int channel_index = is_channel_active(array[1]);
+	if (channel_list[channel_index].user_exists(user_list[USER_ID]))
+		return ;
+	channel_list[channel_index].add_to_channel(user_list[USER_ID]);
+	send_msg(pfds[index].fd, create_msg_2(index, array[0] + " " + array[1])); 
+	send_msg(pfds[index].fd, create_msg(index, "331", array[1] + " :" + channel_list[channel_index].get_topic()));
+	send_msg(pfds[index].fd, create_msg(index, "353", "= " + array[1] + " :" + channel_list[channel_index].get_str_user_list()));
+	send_msg(pfds[index].fd, create_msg(index, "366", array[1] + " :End of NAMES list"));
+	channel_list[channel_index].send_message(user_list[USER_ID], create_msg_2(index, array[0] + " " + array[1]));
+
+}
+
+void Server::command_topic(int index, std::vector<std::string> array){
+	Channel channel = find_channel(array[1]);
+	channel.set_topic(array[2].substr(1));
+	std::string message = create_msg_2(index, "TOPIC " + array[1] + " " + array[2]);
+	channel.send_message(user_list[USER_ID], message);
+	write(user_list[USER_ID].get_fd(), message.c_str(), message.length());
+}
+
+void Server::command_who(int index, std::vector<std::string> array){
+	Channel channel = find_channel(array[1]);
+	std::vector<User> channels_user = channel.get_user_list();
+	for (std::vector<User>::const_iterator it = channels_user.begin(); it != channels_user.end(); ++it) {
+		send_msg(pfds[index].fd, create_msg(index, "352", (*it).get_user_name() + " " + host_name + " " + host_name + " " + (*it).get_nick_name() + " H :0 " + (*it).get_real_name())); //H AND :0 could change
+	}
+	send_msg(pfds[index].fd, create_msg(index, "315", array[1] + " :End of WHO list"));
+
+}
+
+void Server::command_part(int index, std::vector<std::string> array){
+	find_channel(array[1]).remove_from_channel(user_list[USER_ID]);
+	std::vector<User> channels_user = find_channel(array[1]).get_user_list();
+	for (std::vector<User>::const_iterator it = channels_user.begin(); it != channels_user.end(); ++it) {
+		send_msg((*it).get_fd(), create_msg_2(index, array[0] + " " + array[1] + " :" + user_list[USER_ID].get_nick_name()));
+	}
+}
+
+void Server::command_quit(int index){
+	remove_from_all_channels(user_list[USER_ID], index);
+	delete_fd(index);
+	delete_user(index);
+}
+
+
+// USER/FD CREATE/DELETE
+void Server::create_user(std::string user_name, std::string nick_name, std::string real_name, int fd){
+	if (is_nickname_exist(nick_name) == -1) {
+		User new_user(user_name, nick_name, real_name, fd);
+		user_list.push_back(new_user);
+	} else {
+		// send error code
+	}
+}
+
+void Server::create_fd(int fd) {
+	struct pollfd temp;
+	temp.fd = fd;
+	temp.events = POLLIN;
+	temp.revents = 0;
+
+	pfds.push_back(temp);
+}
+
+void Server::delete_user(int index) {
+		user_list.erase(user_list.begin() + USER_ID);
+}
+
+void Server::delete_fd(int index) {
+	close(pfds[index].fd);
+	pfds.erase(pfds.begin() + index);
+}
+
+// GETTERS
+std::string Server::get_host_name() {
+	return host_name;
+}
+
+//SETTER
+void Server::set_host_name() {
+	char tmp[255];
+	gethostname(tmp, 255);
+	host_name = tmp;
+}
+
+void Server::set_time(){
+	time_t rawtime;
+	struct tm *timeinfo;
+	char buffer[80];
+
+	std::time(&rawtime);
+	timeinfo = localtime(&rawtime);
+
+	strftime(buffer, 80, "%d-%m-%Y %I:%M:%S", timeinfo);
+	created_time = std::string(buffer);
 }
 
 // DEBUG
